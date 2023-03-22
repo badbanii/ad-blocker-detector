cfd5ee3f684ebf585fb62c99a04cb181a93ffdf5
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-22 23:19:38 +0200

    Added adblock detection

--- app/src/main/java/com/abetterandroid/adblockerdetector/MainActivity.kt
+++ app/src/main/java/com/abetterandroid/adblockerdetector/MainActivity.kt
@@ -3,6 +3,7 @@ package com.abetterandroid.adblockerdetector
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,26 +11,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.abetterandroid.adblockerdetector.ui.mainscreen.MainScreen
+import com.abetterandroid.adblockerdetector.ui.mainscreen.MainViewModel
 import com.abetterandroid.adblockerdetector.ui.theme.AdBlockerDetectorTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkForAdBlockers()
+        setBarColors()
         setContent {
             MyApp {
-                MainScreen()
+                MainScreen(viewModel)
             }
+        }
+    }
+    private fun checkForAdBlockers()
+    {
+        viewModel.checkForAdBlocker(this.baseContext)
+    }
+    private fun setBarColors()
+    {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        window.statusBarColor = resources.getColor(R.color.black)
+        window.navigationBarColor=resources.getColor(R.color.black)
+    }
+    @Composable
+    fun MyApp(content: @Composable () -> Unit) {
+        MaterialTheme {
+            content()
+        }
     }
 }
-@Composable
-fun MyApp(content: @Composable () -> Unit) {
-    MaterialTheme {
-        content()
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-        MainScreen()
-}}
--- app/src/main/java/com/abetterandroid/adblockerdetector/ui/mainscreen/MainScreen.kt
+++ app/src/main/java/com/abetterandroid/adblockerdetector/ui/mainscreen/MainScreen.kt
@@ -1,29 +1,93 @@
 package com.abetterandroid.adblockerdetector.ui.mainscreen
 
+import android.os.Process
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun MainScreen() {
-  //  val hasAdBlocker by viewModel.hasAdBlocker.observeAsState(initial = null)
-val hasAdBlocker=true
+fun MainScreen(viewModel: MainViewModel) {
+    val hasAdBlocker by viewModel.hasAdBlocker.observeAsState(initial = false)
+    val adBlockName by viewModel.adBlockerName.observeAsState(initial = "")
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        if (hasAdBlocker == true) {
-            Text(text = "An ad blocker is installed on this device.")
-        } else if (hasAdBlocker == false) {
-            Text(text = "No ad blocker detected.")
-        } else {
-            // Placeholder text while the ad blocker check is in progress
-            Text(text = "Checking for ad blockers...")
+        Row(modifier =
+        Modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp)
+            .padding(horizontal = 20.dp),
+            horizontalArrangement =
+            Arrangement.SpaceBetween
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Menu")
+            }
+            Button(
+                onClick = { Process.killProcess(Process.myPid())}) {
+                Text(text = "Quit")
+            }
+        }
+        Column(modifier = Modifier.padding(horizontal = 20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Box(modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .background(Color.Magenta, RoundedCornerShape(40.dp))
+            )
+            {
+                Text(
+                    text = if (hasAdBlocker) "Adblocker is active! \n<$adBlockName>" else "Couldn't find any adblockers using package manager. If you see ads, no adblocker is enabled.",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(all = 20.dp)
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                )
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp)
+                .padding(top=20.dp)) {
+                Text(text = "Test random ad")
+            }
+        }
+
+        Box(modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .padding(bottom = 20.dp)
+            .fillMaxWidth()
+            .background(Color.Magenta, RoundedCornerShape(40.dp))
+        )
+        {
+            Text(
+                text = "Banner placeholder",
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(all = 20.dp),
+                textAlign = TextAlign.Center,
+            )
         }
     }
 }
